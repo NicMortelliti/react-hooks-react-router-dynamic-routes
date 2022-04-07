63b8c9bea224c5d0f42204858b345c49cb227519
NicMortelliti <npmortelliti@gmail.com>
2022-04-07 11:15:09 -0700

    Import new MovieShow component. Add route handling.

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* 
+      We can use the current URL from the 'match' object as part of the path;
+      This will generate a url like "/movies/:movieId"
+       */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
